3c5195705feb462cd5457241eb1acb89aa9cac75
numcayX2 <namchayshop@gmail.com>
2022-05-16 18:22:05 +0700

    HaHa
    1.7

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ function btn1() {
    var element4 = document.getElementById("btn2");
    element4.classList.toggle("btn2-none");
    var element5 = document.getElementById("btn1");
-   element5.classList.toggle("btn1-active")
+   element5.classList.toggle("btn1-active");
+                      document.getElementById("btn1").style.padding = "20px"; 
+                      document.getElementById("btn1").style.borderRadius = "60px"; 
+                      document.getElementById("btn1").style.marginTop = "20px";
+                        console.log("done btn1-1!");
+    setTimeout(() => { document.getElementById("btn1").style.padding = "35px"; 
+                       document.getElementById("btn1").style.borderRadius = "5px";
+                       document.getElementById("btn1").style.marginTop = "10px";
+                        console.log("done btn1-2!");}, 125);                    
 }
 
 function btn2() {
@@ -16,6 +24,14 @@ function btn2() {
    element3.classList.toggle("btn1-none");
    var element6 = document.getElementById("btn2");
    element6.classList.toggle("btn2-active");
+                      document.getElementById("btn2").style.padding = "20px"; 
+                      document.getElementById("btn2").style.borderRadius = "60px"; 
+                      document.getElementById("btn2").style.marginTop = "20px";
+                        console.log("done btn2-1!");
+    setTimeout(() => { document.getElementById("btn2").style.padding = "35px"; 
+                       document.getElementById("btn2").style.borderRadius = "5px";
+                       document.getElementById("btn2").style.marginTop = "10px";
+                        console.log("done btn2-2!");}, 125); 
 }
 
 
