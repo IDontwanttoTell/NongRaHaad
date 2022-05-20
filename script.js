30a59123ccf6d27cd7ea980b2a002a2512eafaf0
numcayX2 <namchayshop@gmail.com>
2022-05-20 23:50:57 +0700

    HaHa
    fix button actived

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ function btn1() {
    element.classList.toggle("content1-active");
    var element4 = document.getElementById("btn2");
    element4.classList.toggle("btn2-none");
-   var element5 = document.getElementById("btn1");
-   element5.className = "btn1-active";
 }
 
 function btn2() {
@@ -16,8 +14,6 @@ function btn2() {
    element1.classList.toggle("content2-active");
    var element3 = document.getElementById("btn1");
    element3.classList.toggle("btn1-none");
-   var element6 = document.getElementById("btn2");
-   element6.className = "btn2-active";
 }
 
 
